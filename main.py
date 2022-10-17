d90d7d15002730418fe860498739eac99aa609eb
Woopertail <woopertail@gmail.com>
2022-10-17 14:20:37 +0300

    Added "§r" to the end of each item name. Added credits in each .lang file.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from rarity_table import rarity_table
 
 colorama.init()
 
-
 rarity_colors = {
     0: "",  # no rarity
     1: "§7",  # common
@@ -18,34 +17,53 @@ rarity_colors = {
     6: "§6§l",  # relic
 }
 
-file_name = input("Enter filename: ") + ".lang"
+f_credits = """## This file is created by Woopertail.
+## To download or update more add-ons, visit next links:
+##
+## MCPEDL: mcpedl.com/user/woopertail/
+## Discord: discord.gg/hYtZMEfZSE 
+##
+## You able to:
+## Include this file in your add-on.
+## Modify this file, include modified file in your add-on.
+## Use this file on public servers or realms.
+## Do with this file whatever you want, in general.
+##
+## BUT
+## do not remove my credits (this text)
+## do not edit my credits (this text)"""
 
-if file_name == "all.lang":
-    file_name = os.listdir("langs\\old")
-else:
-    file_name = [file_name]
+files = input("Enter filename: ") + ".lang"
 
-for file in file_name:
+files = os.listdir("langs\\old") if files == "all.lang" else [files]
+
+for file in files:
     if not os.path.exists(f"langs\\old\\{file}"):
-        print("Файл не найден.")
+        print(f"Файл {file} не найден.")
         sys.exit()
 
     with open(f"langs\\old\\{file}", "r", encoding="utf-8") as f:
         old_file = f.read()
 
     with open(f"langs\\new\\{file}", "w", encoding="utf-8") as f:
+        f.write(f"{f_credits}\n\n\n\n")
         for line in old_file.split("\n"):
-            if not line.split("=")[0] in rarity_table:
+            item_id = line.split("=")[0]
+            if item_id not in rarity_table or rarity_table[item_id] == 0:
                 print(f"{Fore.RED}{line}{Style.RESET_ALL}")
                 continue
 
-            if not rarity_table[line.split("=")[0]] in rarity_colors:
-                color = rarity_table[line.split("=")[0]]
+            if not rarity_table[item_id] in rarity_colors:
+                color = rarity_table[item_id]
             else:
-                color = rarity_colors[rarity_table[line.split('=')[0]]]
+                color = rarity_colors[rarity_table[item_id]]
 
-            new_line = line.replace("=", f"={color}")
+            new_line = line.replace("=", f"={color}")\
+                .replace("#", "")\
+                .strip()\
+                + "§r"
             f.write(f"{new_line}\n")
             print(f"{Fore.GREEN}{new_line}{Style.RESET_ALL}")
+        f.write(f"\n\n\n{f_credits}")
 
     print(f"\n\n\n\n\n\n{Fore.GREEN}Done.{Style.RESET_ALL}")
